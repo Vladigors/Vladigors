5aadce09cfb4ddd6978deeb087e349ab3e966acf
Vladigors <biutycool@bk.ru>
2021-06-04 23:24:40 +0300

    add function mul add print mul delete text add text.upper in strings.py

--- func/nums.py
+++ func/nums.py
@@ -2,13 +2,15 @@ def plus(a, b):
     return a + b
 
 
-text = 'new text'
-
-
 def minus(a, b):
     return a - b
+
+
+def mul(a, b):
+    return a * b
 
 
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
+    print(mul(3, 3))
