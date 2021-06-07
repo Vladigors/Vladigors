c5d33d78e5ede58cdff1327dcfecf2f682f55423
Vladigors <83923210+Vladigors@users.noreply.github.com>
2021-06-07 21:52:53 +0300

    Add files via upload

---
+++ nums.py
@@ -0,0 +1,16 @@
+def plus(a, b):
+    return a + b
+
+
+def minus(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+if __name__ == '__main__':
+    print(plus(2, 2))
+    print(minus(4, 2))
+    print(mul(3, 3))
